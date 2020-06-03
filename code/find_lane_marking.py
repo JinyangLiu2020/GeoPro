4c7612bf226f583e028a49a8a43ccd18957acd47
Jinyang Liu <838152506@qq.com>
2020-06-03 16:29:41 -0500

    add line finder in find_lane

--- code/find_lane_marking.py
+++ code/find_lane_marking.py
@@ -14,6 +14,48 @@ def find_lane_marking(file_path, size = (512,512)):
                 mask[i,j] = 0
     return mask
 
+def find_lanes(mask,size = (1024,1024)):
+    kernel = cv2.getStructuringElement(cv2.MORPH_RECT,(3,3))
+    mask = cv2.erode(mask,kernel)
+    mask = cv2.dilate(mask,kernel)
+    _, labels, stats, centroids = cv2.connectedComponentsWithStats(mask)
+    lanes = []
+    pics = []
+    for i in range(1,len(stats)):
+        label = (labels==i).astype(np.uint8)
+        y_sum, x_sum = label.sum(axis = 0), label.sum(axis = 1)
+        x,y_start_arr,y_end_arr = [],[],[]
+        for i in range(len(x_sum)):
+            if x_sum[i] != 0:
+                x.append(i)
+        x_start,x_end = x[len(x)//20],x[len(x)*19//20]
+        for i in range(len(label[x_start])):
+            if label[x_start][i] != 0:
+                y_start_arr.append(i)
+            if label[x_end][i] != 0:
+                y_end_arr.append(i)
+        y_start = y_start_arr[len(y_start_arr)//2]
+        y_end = y_end_arr[len(y_end_arr)//2]
+        start = (y_start,x_start)
+        end = (y_end,x_end)
+        lanes.append([start,end])
+        pics.append(label)
+    return lanes,pics
+
+
 mask = find_lane_marking('..\\data\\image\\front.jpg',(1024,1024))
-cv2.imshow('mask',mask)
-cv2.waitKey()
+lanes,pics = find_lanes(mask)
+
+'''
+for i in range(1,len(lanes)):
+    label = pics[i]
+    ptStart = lanes[i][0]
+    ptEnd = lanes[i][1]
+    point_color = (0, 255, 0) # BGR
+    thickness = 1
+    lineType = 4
+    img = cv2.cvtColor(label*255, cv2.COLOR_GRAY2RGB);
+    cv2.line(img, ptStart, ptEnd, point_color, thickness, lineType)
+    cv2.imshow('img',img)
+    cv2.waitKey()
+'''
