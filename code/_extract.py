fa81d80613d70fbcfed0e90a769ffedaecbaa8fd
imdonnie <donniexero@gmail.com>
2020-06-03 00:19:15 -0500

    cluster

--- code/_extract.py
+++ code/_extract.py
@@ -21,9 +21,55 @@ def findLaneMarking(filename):
     cv2.imshow('img', dilation)
     cv2.waitkey(0)
 
+def clusterLaneMarking(filename):
+    points = np.loadtxt(filename)
+    points = points[:,:]
+    from sklearn.cluster import KMeans
+    kmeans = KMeans(n_clusters=7, random_state=0).fit(points)
+    labels = kmeans.labels_
+    # np.savetxt('lables', labels)
+    color_cluster = []
+    colors = [[240,240,240],[240,0,0],[0,240,0],[0,0,240],[240,240,0],[240,0,240],[0,240,240]]
+    for i in range(points.shape[0]):
+        color = colors[labels[i]]
+        colored_point = []
+        for value in points[i][:-1]:
+            colored_point.append(value)
+        for value in color:
+            colored_point.append(value)
+        # print(colored_point)
+        color_cluster.append(colored_point)
+    np.savetxt('color_cluster.txt', np.array(color_cluster))
+
+        
+
 def findLaneMarking(filename):
     img = cv2.imread(filename)
-    kernel = np.ones((3,3),np.uint8)
+    kernel = np.ones((10,10),np.uint8)
     dilation = cv2.dilate(img, kernel, iterations=3)
-    cv2.imwrite('dialtion.jpg', dilation)
-    # cv2.waitKey(0)
+    # cv2.imwrite('dialtion.jpg', dilation)
+    # dilation = cv2.imread('perfect.jpg')
+
+    # imgray = cv2.cvtColor(dilation, cv2.COLOR_BGR2GRAY)
+    # ret, thresh = cv2.threshold(imgray, 127, 255, 0)
+    # contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    # cv2.drawContours(dilation, contours, -1, (0,120,0), 3)
+    # print(contours, hierarchy)
+    # cv2.imwrite('im2.jpg', dilation)
+
+    edges = cv2.Canny(dilation,0,250,apertureSize = 3)
+    cv2.imwrite('edges.jpg', edges)
+    lines = cv2.HoughLines(edges,1,np.pi/180,50)
+    for rho,theta in lines[0]:
+        a = np.cos(theta)
+        b = np.sin(theta)
+        x0 = a*rho
+        y0 = b*rho
+        x1 = int(x0 + 1000*(-b))
+        y1 = int(y0 + 1000*(a))
+        x2 = int(x0 - 1000*(-b))
+        y2 = int(y0 - 1000*(a))
+
+        cv2.line(dilation,(x1,y1),(x2,y2),(0,0,255),2)
+
+    cv2.imwrite('houghlines3.jpg',dilation)
--- code/main.py
+++ code/main.py
@@ -4,11 +4,12 @@ import cv2
 from mpl_toolkits.mplot3d import Axes3D
 import numpy as np
 from _transform import *
-from _extract import findLaneMarking
+from _extract import findLaneMarking, clusterLaneMarking
 
 if __name__ == "__main__":
     # transPoints()
     # threshold(226, 100, 15)
     # drawPoints()
-    findLaneMarking('back_equ.png')
+    # findLaneMarking('back_equ.png')
+    clusterLaneMarking('lane_marking_int.txt')
     pass
