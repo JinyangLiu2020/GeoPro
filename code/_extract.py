5a43fbec601dfc171b51f95819f39404de71b67b
imdonnie <donniexero@gmail.com>
2020-06-02 22:28:17 -0500

    finish

--- code/_extract.py
+++ code/_extract.py
@@ -1,8 +1,10 @@
 # Camera location:45.90414414, 11.02845385
 import numpy as np
+import cv2
 
 def calDis(x1,y1,x2=9.404743650520686e-11,y2=-1.7409718111593975e-10):
     return ((x1-x2)**2+(y1-y2)**2)**0.5
+
 def threshold(altitude, greyscale, distance):
     points_int = np.loadtxt('points_int.txt')
     lane_markings = []
@@ -11,3 +13,17 @@ def threshold(altitude, greyscale, distance):
             lane_markings.append(line)
     lane_markings = np.array(lane_markings)
     np.savetxt('lane_marking_int.txt', lane_markings)
+
+def findLaneMarking(filename):
+    img = cv2.imread(filename)
+    kernel = np.ones((3,3),np.uint8) 
+    dilation = cv2.dilate(img, kernel, iterations=3)
+    cv2.imshow('img', dilation)
+    cv2.waitkey(0)
+
+def findLaneMarking(filename):
+    img = cv2.imread(filename)
+    kernel = np.ones((3,3),np.uint8)
+    dilation = cv2.dilate(img, kernel, iterations=3)
+    cv2.imwrite('dialtion.jpg', dilation)
+    # cv2.waitKey(0)
--- code/main.py
+++ code/main.py
@@ -3,11 +3,12 @@ import math
 import cv2
 from mpl_toolkits.mplot3d import Axes3D
 import numpy as np
-from code._transform import *
-from code._extract import *
+from _transform import *
+from _extract import findLaneMarking
 
 if __name__ == "__main__":
     # transPoints()
     # threshold(226, 100, 15)
     # drawPoints()
+    findLaneMarking('back_equ.png')
     pass
