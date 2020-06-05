4dadcccb04fc29f54b26909e9db077410f6653df
imdonnie <donniexero@gmail.com>
2020-06-05 03:20:14 -0500

    clean paths

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ import math
 import cv2
 from mpl_toolkits.mplot3d import Axes3D
 import numpy as np
-from _transform import *
-from _extract import *
+from code._transform import *
+from code._extract import *
 
 if __name__ == "__main__":
     # transPoints()
@@ -19,18 +19,18 @@ if __name__ == "__main__":
     x_i, y_i, direction = cc_to_ic(x_c, y_c, z_c)
     print(x_i, y_i, direction)
 
-    back = cv2.imread('back.png')
+    back = cv2.imread('tmp/back.png')
     for i in range(1035, 1055):
         for j in range(1025, 1045):
             back[i, j] = [0, 0, 255]
-    cv2.imwrite('back_inter.png', back)
+    cv2.imwrite('tmp/back_inter.png', back)
 
     x_c, y_c, z_c = enu_to_cc(937.0089799531036, 1775.5955312868261, -2.52)
     x_i, y_i, direction = cc_to_ic(x_c, y_c, z_c)
     print(x_i, y_i, direction)
 
-    back = cv2.imread('front.png')
+    back = cv2.imread('tmp/front.png')
     for i in range(995, 1015):
         for j in range(1025, 1045):
             back[i, j] = [0, 0, 255]
-    cv2.imwrite('front_inter.png', back)
+    cv2.imwrite('tmp/front_inter.png', back)
