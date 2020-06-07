5f12d0fd7094f74efc68ab10a32c014e25ba8310
GYX97 <413998386@qq.com>
2020-06-06 22:01:59 -0500

    'misallignment'

--- code/misallignment.py
+++ code/misallignment.py
@@ -4,7 +4,10 @@ import math
 def pointwise(point_cloud,rgb):
     x = rgb[0]-point_cloud[0]
     y = rgb[1]-point_cloud[1]
-    return [x,y]
+    matrix = [1,0,x;
+              0,1,y;
+              0,0,1]
+    return matrix
     
 def linewise(point_cloud,rgb):
     mid_point = [1024,point_cloud[0]*1024+point_cloud[1]]
@@ -16,5 +19,11 @@ def linewise(point_cloud,rgb):
     angledif = angle2-angle1
     new_mid_point = [1024,rgb[0]*1024+point_cloud[1]]
     shift = pointwise(new_mid_point,mid_rgb)
-    return shift, angledif
-    #shift = pointwise(mid_point,mid_rgb)
+    rotation_matrix = [cos(angledif),sin(angleif),0;
+                      sin(angledif),cos(angleif),0;
+                      0            ,0           ,1]
+    shift_matrix = [1,0,shift[0];
+                    0,1,shift[1];
+                    0,0,1]
+    matrix = shift_matrix*rotation_matrix
+    return matrix
